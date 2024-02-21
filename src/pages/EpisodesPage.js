76c4adc519f033d4ee6e6fbf4beeb1db97699641
IrinaHol <irinagolovatska@i.ua>
2024-02-21 19:51:48 +0300

    fix

--- src/pages/EpisodesPage.js
+++ src/pages/EpisodesPage.js
@@ -1,11 +1,8 @@
-import {Outlet} from "react-router-dom";
-
 import {Episodes} from "../components";
 
 const EpisodesPage = () => {
     return (
         <div>
-            <Outlet/>
             <Episodes/>
         </div>
     );
