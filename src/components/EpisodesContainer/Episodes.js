503b314f7668dd82babbd713dc0bb4754add1376
IrinaHol <irinagolovatska@i.ua>
2024-02-21 20:36:07 +0300

    fix

--- src/components/EpisodesContainer/Episodes.js
+++ src/components/EpisodesContainer/Episodes.js
@@ -1,35 +1,39 @@
 import {useEffect, useState} from "react";
-import {useSearchParams} from "react-router-dom";
+// import {useSearchParams} from "react-router-dom";
 
 import {episodeService} from "../../services";
 import {Episode} from "./Episode";
 import css from "./Episode.module.css";
+import {usePageQuery} from "../../hooks";
 
 const Episodes = () => {
     const [episodes, setEpisodes] = useState([])
-    const [query, setQuery] = useSearchParams({page: '1'});
+    // const [query, setQuery] = useSearchParams({page: '1'});
     const [prevNext, setPrevNext] = useState({prev: null, next: null})
+    const {page, prevPage, nextPage} = usePageQuery()
+    console.log(page)
 
     useEffect(() => {
-        episodeService.getAll(query.get('page')).then(({data: {results, info}}) => {
+        episodeService.getAll(page).then(({data: {results, info}}) => {
             setEpisodes(results)
             setPrevNext({prev: info.prev, next: info.next})
         })
-    }, [query]);
+    }, [page]);
 
-    const prev = () => {
-        setQuery(prev => {
-            prev.set('page', (+prev.get('page') - 1).toString())
-            return prev
-        })
-    }
 
-    const next = () => {
-        setQuery(prev => {
-            prev.set('page', (+prev.get('page') + 1).toString())
-            return prev
-        })
-    }
+    // const prev = () => {
+    //     setQuery(prev => {
+    //         prev.set('page', (+prev.get('page') - 1).toString())
+    //         return prev
+    //     })
+    // }
+
+    // const next = () => {
+    //     setQuery(prev => {
+    //         prev.set('page', (+prev.get('page') + 1).toString())
+    //         return prev
+    //     })
+    // }
 
     return (
         <div>
@@ -37,8 +41,8 @@ const Episodes = () => {
                 {episodes.map(episode => <Episode key={episode.id} item={episode}/>)}
             </div>
             <div className={css.btn}>
-                <button disabled={!prevNext.prev} onClick={prev}>prevPage</button>
-                <button disabled={!prevNext.next} onClick={next}>nextPage</button>
+                <button disabled={!prevNext.prev} onClick={prevPage}>prevPage</button>
+                <button disabled={!prevNext.next} onClick={nextPage}>nextPage</button>
             </div>
         </div>
     );
